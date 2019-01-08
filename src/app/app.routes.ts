5d908cabb4e0a58efe147fcd6c0e19c4683e5843
Rafael Noberto De Franca <rnfranca@stefanini.com>
2019-01-08 09:10:07 -0200

    criando component restaurant-details e passando parametros

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import {HomeComponent} from './home/home.component'
 import { AboutComponent } from './about/about.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 
 export const ROUTER= [
     {
@@ -10,7 +11,12 @@ export const ROUTER= [
     {
         path:'about',
         component: AboutComponent
-    },{
+    },
+    {
+        path:'restaurantes/:id',
+        component: RestaurantDetailComponent
+    },
+    {
         path:'restaurantes',
         component: RestaurantsComponent
     }
