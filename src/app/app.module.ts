5910e6e19dda7f6d1860f3d63f40c219f2c3df77
Rafael Noberto De Franca <rnfranca@stefanini.com>
2019-01-10 09:30:59 -0200

    Implementado ng-Content dos inputs e tratamento de errors dinamicos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.compo
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.serv';
 import { OrderComponent } from './order/order.component';
+import { InputComponent } from './shared/input/input.component';
 
 
 @NgModule({
@@ -36,7 +37,8 @@ import { OrderComponent } from './order/order.component';
     ShoppingCartComponent,
     MenuItemComponent,
     ReviewsComponent,
-    OrderComponent
+    OrderComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
