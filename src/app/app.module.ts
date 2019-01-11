a909c9fcd6420f3a6f10dad9dd0657940dbcffac
Rafael Noberto De Franca <rnfranca@stefanini.com>
2019-01-11 09:45:31 -0200

    Implementando componente com os itens de compra

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,7 @@ import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-
 import { OrderComponent } from './order/order.component';
 import { InputComponent } from './shared/input/input.component';
 import { RadioComponent } from './shared/radio/radio.component';
+import { OrderItemsComponent } from './order/order-items/order-items.component';
 
 
 @NgModule({
@@ -40,7 +41,8 @@ import { RadioComponent } from './shared/radio/radio.component';
     ReviewsComponent,
     OrderComponent,
     InputComponent,
-    RadioComponent
+    RadioComponent,
+    OrderItemsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/order/order-items/order-items.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
+
+@Component({
+  selector: 'mt-order-items',
+  templateUrl: './order-items.component.html'
+})
+export class OrderItemsComponent implements OnInit {
+@Input() items: CartItem[]
+
+@Output() increaseQty = new EventEmitter<CartItem>()
+@Output() decreaseQty = new EventEmitter<CartItem>()
+@Output() remove = new EventEmitter<CartItem>()
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  emityIncreaseQty(item:CartItem){
+    this.increaseQty.emit(item)
+  }
+
+  emityDecreaseQty(item:CartItem){
+    this.decreaseQty.emit(item)
+  }
+
+  emityRemove(item:CartItem){
+    this.remove.emit(item)
+  }
+
+}
