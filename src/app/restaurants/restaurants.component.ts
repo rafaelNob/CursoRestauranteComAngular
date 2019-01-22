06888020a96d313b69dd948bd1cedce47b2feeb5
Rafael Noberto De Franca <rnfranca@stefanini.com>
2019-01-22 10:32:25 -0200

    animação com void e estilos em trasições (restaurant)

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -2,12 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { Restaurant } from './restaurant/restaurante.model';
 import { RestaurantService } from './restaurants.service';
 
+
 @Component({
   selector: 'mt-restaurants',
   templateUrl: './restaurants.component.html',
   styleUrls: ['./restaurants.component.css']
 })
 export class RestaurantsComponent implements OnInit {
+
+
   restaurants: Restaurant[]
   constructor( private restService: RestaurantService) { }
 
