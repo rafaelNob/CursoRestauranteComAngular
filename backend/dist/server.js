c6864510c0fa9be4367f2f651f416b250c78462f
Rafael Noberto De Franca <rnfranca@stefanini.com>
2019-01-28 08:45:32 -0200

    configurando tsconfig.json e server.js

---
+++ backend/dist/server.js
@@ -0,0 +1,25 @@
+"use strict";
+exports.__esModule = true;
+var jsonServer = require("json-server"); //modulo que ele esta importando
+var fs = require("fs"); //capaz de ler arquivos do disco
+var https = require("https");
+var server = jsonServer.create(); //faz uma tipagem de Expressa para poder trazer metodos e tratamentos de erros
+var router = jsonServer.router('db.json');
+var middlewares = jsonServer.defaults();
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares);
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser);
+// Use default router
+server.use(router);
+//obtendo referencia ao certificado e a chave
+var options = {
+    cert: fs.readFileSync('./backend/keys/cert.pem'),
+    key: fs.readFileSync('./backend/keys/key.pem')
+};
+https.createServer(options, server)
+    .listen(3002, function () {
+    console.log('JSON Server is running on https://localhost:3001'); //criando o servidor
+});
+//node backend/dist/server
---
+++ backend/server.ts
@@ -0,0 +1,32 @@
+import * as jsonServer from 'json-server' //modulo que ele esta importando
+import {Express} from 'express' 
+
+import * as fs from 'fs' //capaz de ler arquivos do disco
+import * as https from 'https' 
+
+const server:Express = jsonServer.create() //faz uma tipagem de Expressa para poder trazer metodos e tratamentos de erros
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares)
+
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser)
+
+// Use default router
+server.use(router)
+
+//obtendo referencia ao certificado e a chave
+const options ={
+  cert: fs.readFileSync('./backend/keys/cert.pem'),
+  key: fs.readFileSync('./backend/keys/key.pem')
+}
+
+https.createServer(options,server) 
+.listen(3002, () => {
+  console.log('JSON Server is running on https://localhost:3001') //criando o servidor
+})
+
+//node backend/dist/server
